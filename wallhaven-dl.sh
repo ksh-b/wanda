9bae68af01b834aae6e1bed82be8730a3ab0e497
ksyko <lord.kratos.kb19@gmail.com>
2021-05-13 11:57:26 +0530

    remove unused

--- wallhaven-dl.sh
+++ wallhaven-dl.sh
@@ -1,7 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-mkdir -p wallpaper
 . ./wallhaven.config
-cd wallpaper && rm *
 api="https://wallhaven.cc/api/v1/search?apikey=$api_key&categories=$categories&purity=$purity&ratios=$ratios&page=$page&sorting=$sorting&order=$order&topRange=$topRange&atleast=$atleast&resolutions=$resolutions&colors=$colors&seed=$seed"
 url=$(curl -s $api | jq --raw-output '.data[0].path')
 case $screen in
