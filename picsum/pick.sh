c728f6a9b61d35b919bdc9b6feaa0c49eaf23224
ksyko <lord.kratos.kb19@gmail.com>
2021-05-13 22:07:06 +0530

    remove unessecary line

--- picsum/pick.sh
+++ picsum/pick.sh
@@ -1,6 +1,5 @@
 . ./picsum/config
 seed=$(xxd -l 8 -c 8 -p < /dev/random)
 api="https://picsum.photos/seed/$seed/$width/$height"
-echo $api
 url=$(curl -Ls -o /dev/null -w %{url_effective} $api)
 echo $url
