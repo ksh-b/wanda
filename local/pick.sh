55eebf1933b852b8cc0f2e58d7201dda6fb1b9c3
ksyko <lord.kratos.kb19@gmail.com>
2021-05-13 23:29:14 +0530

    add local

---
+++ local/pick.sh
@@ -0,0 +1,4 @@
+#!/data/data/com.termux/files/usr/bin/bash
+. ./local/config
+filepath=$(find "$images_path" -type f -exec file --mime-type {} \+ | awk -F: '{if ($2 ~/image\//) print $1}' | shuf -n 1)
+echo $filepath
--- wanda.sh
+++ wanda.sh
@@ -1,14 +1,30 @@
 . ./wanda.config
 . ./$source/pick.sh
-case $screen in
-    both)
-        termux-wallpaper -u $url
-        termux-wallpaper -lu $url
-        ;;
-    home)
-        termux-wallpaper -u $url
-        ;;
-    lock)
-        termux-wallpaper -lu $url
-        ;;
-esac
+
+if [ "$source" = "local" ]; then
+  case $screen in
+      both)
+          termux-wallpaper -f $filepath
+          termux-wallpaper -lf $filepath
+          ;;
+      home)
+          termux-wallpaper -f $filepath
+          ;;
+      lock)
+          termux-wallpaper -lf $filepath
+          ;;
+  esac
+else
+  case $screen in
+      both)
+          termux-wallpaper -u $url
+          termux-wallpaper -lu $url
+          ;;
+      home)
+          termux-wallpaper -u $url
+          ;;
+      lock)
+          termux-wallpaper -lu $url
+          ;;
+  esac
+fi
