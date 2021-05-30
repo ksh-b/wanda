959c73b3917af638e301671ed376758b5cef8b32
ksyko <ksyko@pm.me>
2021-05-30 18:02:48 +0530

    pick random page

--- wallhaven/pick.sh
+++ wallhaven/pick.sh
@@ -1,15 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo $SCRIPT_DIR
 . $SCRIPT_DIR/wallhaven/config
+
+# get parameters with a value
 keys=(apikey q categories purity sorting order topRange atleast resolutions ratios colors page seed)
 for i in "${keys[@]}";
   do if [ -n "${!i}" ]; then
     params="${params}$i=${!i}&"
   fi
 done
-api="https://wallhaven.cc/api/v1/search?$params"
-res=$(curl -s $api)
-limit=$(echo $res | jq --raw-output '.meta.per_page')
-rand=$(( $RANDOM % limit + 1 ))
+
+# make request with provided parameters
+api="https://wallhaven.cc/api/v1/search?"
+res=$(curl -s "${api}${params}")
+echo $res
+
+# use a random number if sorting is not random
+if [ "$sorting" != "random" ]; then
+  last=$(echo $res | jq --raw-output '.meta.last_page')
+  page=$(shuf -i 1-$last -n 1)
+  res=$(curl -s "${api}${params}&page=$page")
+  limit=$(echo $res | jq --raw-output '.meta.per_page')
+  rand=$(shuf -i 1-$limit -n 1)
+else
+  rand=0
+fi
+
+# get wallpaper link
 url=$(echo $res | jq --raw-output ".data[$rand].path")
 echo $url
