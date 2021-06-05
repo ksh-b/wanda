28bafcb979f66bedeae534e948073c87e7b5ed77
ksyko <ksyko@pm.me>
2021-06-05 19:30:38 +0530

    add offline mode

--- wanda.sh
+++ wanda.sh
@@ -1,8 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-. $SCRIPT_DIR/config
-. $SCRIPT_DIR/$source/pick.sh
+# . $SCRIPT_DIR/config
+# . $SCRIPT_DIR/$source/pick.sh
 
+# try to connect to internet
+curl -s "https://detectportal.firefox.com/success.txt"
+
+# if offline, use local or skip
+if [ $? != 0 ]; then
+  if [ $offline_use_local = "true" ]; then
+    source="local"
+  else
+    exit 0
+  fi
+fi
+
+# set wallpaper according to source
 if [ "$source" = "local" ]; then
   case $screen in
       both)
