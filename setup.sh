d282b305857a4cc7c87f68ce764aa26287deed61
Kshitij B <ksyko@protonmail.com>
2021-06-12 06:13:21 +0000

    merge with dev (#4)
    * add basic script for installation
    
    * pick random page
    
    * change sh to bash
    
    * add offline mode
    
    * add shebang
    
    * add comment
    
    * remove debug code
    
    * add autocrop option
    
    * move api key to config
    
    * set source when using autocrop
    
    * better thread with mobile papes
    
    * add var for original and cropped files
    
    * rearrange
    
    * add shebang
    
    * prevent 429
    
    * add option to keep file
    
    * fix file not removed
    
    * revoke and remove key
    
    * change default value
    
    * change default value
    
    * crop only if width>height
    
    * create download dir if not already
    
    * fix path
    
    * ignore autocrop if api fails
    
    * fix script
    
    * formatting
    
    Co-authored-by: ksyko <ksyko@pm.me>

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # experimental
-wanda_dir="storage/shared/scripts"
 clear
 pkg up
 pkg in termux-api curl jq file
 termux-setup-storage
+cd $HOME
+wanda_dir="$HOME/storage/shared/scripts/wanda/"
 mkdir -p $wanda_dir && cd $wanda_dir
 curl -L -O -s "https://github.com/ksyko/wanda/releases/download/v0.01/wanda.tar.gz" && tar -xzf wanda.tar.gz && echo "Downloaded wanda to $(pwd)" || echo "Failed to download wanda"
+rm wanda.tar.gz
--- wanda.sh
+++ wanda.sh
@@ -7,7 +7,7 @@ curl -s "https://detectportal.firefox.com/success.txt" 1> /dev/null
 
 # if offline, use local or skip
 if [ $? != 0 ]; then
-  if [ $offline_use_local = "true" ]; then
+  if [ "$offline_use_local" = "true" ]; then
     source="local"
   else
     exit 0
@@ -19,23 +19,26 @@ fi
 if [ $autocrop = "true" ]; then
   ofile=$SCRIPT_DIR/downloads/$(basename $url)
   cfile=$SCRIPT_DIR/downloads/cropped/$(basename $url)
-  cropped=$(curl -s --user "$imagga_key" "https://api.imagga.com/v2/croppings?image_url=$url&resolution=${width}x$height")
-  x1=$(echo $cropped |  jq --raw-output ".result.croppings[0].x1")
-  x2=$(echo $cropped |  jq --raw-output ".result.croppings[0].x2")
-  y1=$(echo $cropped |  jq --raw-output ".result.croppings[0].y1")
-  y2=$(echo $cropped |  jq --raw-output ".result.croppings[0].y2")
   curl -s $url --output $ofile
   w=$(identify -format "%w" $ofile)> /dev/null
   h=$(identify -format "%h" $ofile)> /dev/null
   if [ $w -gt $h ]; then
-    convert $ofile -crop ${x2}x${y2}+${x1}+${y1} $cfile
-    source="local"
-    filepath=$cfile
+    cropped=$(curl -s --user "$imagga_key" "https://api.imagga.com/v2/croppings?image_url=$url&resolution=${width}x$height")
+    status=$(echo $cropped |  jq --raw-output ".status.type")
+    if [ $status = "success" ]; then
+      x1=$(echo $cropped |  jq --raw-output ".result.croppings[0].x1")
+      x2=$(echo $cropped |  jq --raw-output ".result.croppings[0].x2")
+      y1=$(echo $cropped |  jq --raw-output ".result.croppings[0].y1")
+      y2=$(echo $cropped |  jq --raw-output ".result.croppings[0].y2")
+      convert $ofile -crop ${x2}x${y2}+${x1}+${y1} $cfile
+      source="local"
+      filepath=$cfile
+    fi
   fi
 fi
 
 # set wallpaper according to source
-if [ "$source" = "local" ]; then
+if [ $source = "local" ]; then
   case $screen in
       both)
           termux-wallpaper -f "$filepath"
@@ -64,7 +67,9 @@ else
 fi
 
 # keep or remove file as per config
-if [ $keep = "false" ]; then rm -f $ofile
+if [ "$keep" = "false" ]; then
+  rm -f $ofile
 fi
-if [ $keep_crop = "false" ]; then rm -f $cfile
+if [ "$keep_crop" = "false" ]; then
+  rm -f $cfile
 fi
