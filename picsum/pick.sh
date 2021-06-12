0f05e3c59ec9ab0815fba08291988bcecf4ac14c
ksyko <ksyko@pm.me>
2021-06-12 14:51:59 +0530

    use double quotes

--- picsum/pick.sh
+++ picsum/pick.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-. $SCRIPT_DIR/picsum/config
+. "$SCRIPT_DIR/picsum/config"
 seed=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')
 api="https://picsum.photos/seed/$seed/$width/$height"
 url=$(curl -Ls -o /dev/null -w %{url_effective} "$api")
