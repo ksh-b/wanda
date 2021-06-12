b1083a5fd0ef8447ea67a466998d819052e35760
ksyko <ksyko@pm.me>
2021-06-12 18:11:46 +0530

    fix script

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ clear
 pkg up
 pkg in termux-api curl jq file
 termux-setup-storage
-cd "$HOME" || echo "Error. Could not navigate to home." && exit 1
+cd "$HOME" || (echo "Error. Could not navigate to home." && exit 1)
 wanda_dir="$HOME/storage/shared/scripts/wanda/"
-mkdir -p "$wanda_dir" && cd "$wanda_dir" || echo "Error. Check storage permissions." && exit 1
-curl -L -O -s "https://github.com/ksyko/wanda/releases/download/v0.01/wanda.tar.gz" && tar -xzf wanda.tar.gz && echo "Downloaded wanda to $(pwd)" || echo "Failed to download wanda"
+(mkdir -p "$wanda_dir" && cd "$wanda_dir") || (echo "Error. Check storage permissions." && exit 1)
+curl -L -O -s "https://github.com/ksyko/wanda/releases/download/v0.03/wanda.tar.gz" && tar -xzf wanda.tar.gz && echo "Downloaded wanda to $(pwd)" || echo "Failed to download wanda"
 mkdir "downloads/cropped/"
 rm wanda.tar.gz
