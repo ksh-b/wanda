00472303747dbefca6b25c75b94a3b735f58b96e
Kshitij B <ksyko@protonmail.com>
2021-06-12 09:30:52 +0000

    merge with dev (#5)
    * use double quotes
    
    Co-authored-by: ksyko <ksyko@pm.me>

--- wallhaven/pick.sh
+++ wallhaven/pick.sh
@@ -1,5 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
-. $SCRIPT_DIR/wallhaven/config
+. "$SCRIPT_DIR/wallhaven/config"
 
 # get parameters with a value
 keys=(apikey q categories purity sorting order topRange atleast resolutions ratios colors page seed)
@@ -15,15 +15,15 @@ res=$(curl -s "${api}${params}")
 
 # use a random number if sorting is not random
 if [ "$sorting" != "random" ]; then
-  last=$(echo $res | jq --raw-output '.meta.last_page')
-  page=$(shuf -i 1-$last -n 1)
+  last=$(echo "$res" | jq --raw-output '.meta.last_page')
+  page=$(shuf -i 1-"$last" -n 1)
   res=$(curl -s "${api}${params}&page=$page")
-  limit=$(echo $res | jq --raw-output '.meta.per_page')
-  rand=$(shuf -i 1-$limit -n 1)
+  limit=$(echo "$res" | jq --raw-output '.meta.per_page')
+  rand=$(shuf -i 1-"$limit" -n 1)
 else
   rand=0
 fi
 
 # get wallpaper link
-url=$(echo $res | jq --raw-output ".data[$rand].path")
-echo $url
+url=$(echo "$res" | jq --raw-output ".data[$rand].path")
+echo "$url"
--- wanda.sh
+++ wanda.sh
@@ -1,12 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-. $SCRIPT_DIR/config
+. "$SCRIPT_DIR/config"
 
 # try to connect to internet
 curl -s "https://detectportal.firefox.com/success.txt" 1> /dev/null
 
 # if offline, use local or skip
-if [ $? != 0 ]; then
+if [ "$mycmd" != 0 ]; then
   if [ "$offline_use_local" = "true" ]; then
     source="local"
   else
@@ -14,23 +14,23 @@ if [ $? != 0 ]; then
   fi
 fi
 
-. $SCRIPT_DIR/$source/pick.sh
+. "$SCRIPT_DIR/$source/pick.sh"
 
-if [ $autocrop = "true" ]; then
-  ofile=$SCRIPT_DIR/downloads/$(basename $url)
-  cfile=$SCRIPT_DIR/downloads/cropped/$(basename $url)
-  curl -s $url --output $ofile
-  w=$(identify -format "%w" $ofile)> /dev/null
-  h=$(identify -format "%h" $ofile)> /dev/null
-  if [ $w -gt $h ]; then
+if [ "$autocrop" = "true" ]; then
+  ofile=$SCRIPT_DIR/downloads/$(basename "$url")
+  cfile=$SCRIPT_DIR/downloads/cropped/$(basename "$url")
+  curl -s "$url" --output "$ofile"
+  w=$(identify -format "%w" "$ofile")> /dev/null
+  h=$(identify -format "%h" "$ofile")> /dev/null
+  if [ "$w" -gt "$h" ]; then
     cropped=$(curl -s --user "$imagga_key" "https://api.imagga.com/v2/croppings?image_url=$url&resolution=${width}x$height")
-    status=$(echo $cropped |  jq --raw-output ".status.type")
-    if [ $status = "success" ]; then
-      x1=$(echo $cropped |  jq --raw-output ".result.croppings[0].x1")
-      x2=$(echo $cropped |  jq --raw-output ".result.croppings[0].x2")
-      y1=$(echo $cropped |  jq --raw-output ".result.croppings[0].y1")
-      y2=$(echo $cropped |  jq --raw-output ".result.croppings[0].y2")
-      convert $ofile -crop ${x2}x${y2}+${x1}+${y1} $cfile
+    status=$(echo "$cropped" |  jq --raw-output ".status.type")
+    if [ "$status" = "success" ]; then
+      x1=$(echo "$cropped" |  jq --raw-output ".result.croppings[0].x1")
+      x2=$(echo "$cropped" |  jq --raw-output ".result.croppings[0].x2")
+      y1=$(echo "$cropped" |  jq --raw-output ".result.croppings[0].y1")
+      y2=$(echo "$cropped" |  jq --raw-output ".result.croppings[0].y2")
+      convert "$ofile" -crop "${x2}x${y2}+${x1}+${y1}" "$cfile"
       source="local"
       filepath=$cfile
     fi
@@ -68,8 +68,8 @@ fi
 
 # keep or remove file as per config
 if [ "$keep" = "false" ]; then
-  rm -f $ofile
+  rm -f "$ofile"
 fi
 if [ "$keep_crop" = "false" ]; then
-  rm -f $cfile
+  rm -f "$cfile"
 fi
