af8524dd59a15c2eb5a6a4698ff8ed35e684e731
ksyko <ksyko@pm.me>
2021-06-27 21:00:48 +0530

    fix for image missing

--- dynamic/pick.sh
+++ dynamic/pick.sh
@@ -1,4 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 . "$SCRIPT_DIR/dynamic/config"
-h=date +"%H"
-echo "$SCRIPT_DIR/dynamic/images/$imageset/$h.jpg"
+h=$(date +"%H")
+filepath="$SCRIPT_DIR/dynamic/images/$imageset/$h.jpg"
+if [ ! -f "$filepath" ]; then
+  h=$((h-1))
+fi
+echo "$filepath"
