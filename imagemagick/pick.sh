0c995295a4d8dd3b1f194cd267c02abe8cd55d63
ksyko <ksyko@pm.me>
2021-06-27 15:46:55 +0530

    remove problematic patterns, add new ones

--- imagemagick/pick.sh
+++ imagemagick/pick.sh
@@ -54,176 +54,158 @@ solid() {
 
 ## Generate a linear gradient wallpaper
 linear_gradient() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color2="$RCOLOR"
-		get_random_number "360"
-		angle="$RNUM"
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color2="$RCOLOR"
+	get_random_number "360"
+	angle="$RNUM"
 	convert -size "$size" -define gradient:angle="$angle" gradient:"$color1-$color2" "$filepath"
 }
 
 ## Generate a radial gradient wallpaper
 radial_gradient() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color="$color1-$RCOLOR"
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color="$color1-$RCOLOR"
 
-		get_random_number "360"
-		angle="$RNUM"
+	get_random_number "360"
+	angle="$RNUM"
 
-		getRandomShape
-		shape="$RSHAPE"
+	getRandomShape
+	shape="$RSHAPE"
 	convert -size "$size" -define gradient:extent="$shape" -define gradient:angle="$angle" radial-gradient:"$color" "$filepath"
 }
 
 ## Generate a twisted gradient wallpaper
 twisted_gradient() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color="$color1-$RCOLOR"
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color="$color1-$RCOLOR"
 
-		get_random_number "500"
-		twist="$RNUM"
+	get_random_number "500"
+	twist="$RNUM"
 	convert -size "$size" gradient:"$color" -swirl "${twist}" "$filepath"
 }
 
 ## Generate a 4 point gradient wallpaper
 bilinear_gradient() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color2="$RCOLOR"
-		get_random_color
-		color3="$RCOLOR"
-		get_random_color
-		color4="$RCOLOR"
-
-		get_random_number "2"
-		if [[ "$RNUM" == "1" ]]; then
-			answer="s"
-		else
-			answer="r"
-		fi
-
-	if [[ $answer == "s" ]] || [[ $answer == "S" ]]; then
-		convert \( xc:"$color1" xc:"$color2" +append \) \( xc:"$color3" xc:"$color4" +append \) -append -size "$size" xc: +swap  -fx 'v.p{i/(w-1),j/(h-1)}' "$filepath"
-	else
-		convert \( xc:"$color1" xc:"$color2" +append \) \( xc:"$color3" xc:"$color4" +append \) -append -filter triangle -resize "$size"\! "$filepath"
-	fi
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color2="$RCOLOR"
+	get_random_color
+	color3="$RCOLOR"
+	get_random_color
+	color4="$RCOLOR"
+	convert \( xc:"$color1" xc:"$color2" +append \) \( xc:"$color3" xc:"$color4" +append \) -append -filter triangle -resize "$size"\! "$filepath"
 }
 
-blurred_noise() {
-		get_random_number "30"
-		blur="$RNUM"
-	convert -size "100x56" xc: +noise Random "/tmp/noise.png"
-	convert "/tmp/noise.png" -virtual-pixel tile -blur 0x30 -auto-level -resize "$size" "$filepath"
+blurred_noise_paint() {
+	convert -size "$size" xc: +noise Random "$filepath"
+	convert "$filepath" -virtual-pixel tile -blur 0x"$blur" -auto-level -paint "$paint" "$filepath"
 }
 
-plasma() {
+plasma_fractal() {
 	get_random_number "360"
 	swirl="$RNUM"
-		get_random_number "15"
-	blur="$RNUM"
 	convert -size "$size"  plasma:fractal \
-				 -blur 0x$blur  -swirl $swirl  "$filepath"
+	-blur 0x"$blur"  -swirl $swirl  "$filepath"
+}
+
+plasma_range() {
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color2="$RCOLOR"
+	convert -size "$size" plasma:$color1-$color2 "$filepath"
+}
+
+plasma_single() {
+	get_random_color
+	color1="$RCOLOR"
+	convert -size "$size" plasma:$color1 "$filepath"
 }
 
 gradient_horizontal() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color2="$RCOLOR"
-		get_random_color
-		color3="$RCOLOR"
-			get_random_number "60"
-		blur="$RNUM"
-	  convert -size "$size" gradient:$color1-$color2 \( +clone +clone \) \
-	          -background $color3 -compose ModulusAdd -blur 0x$blur -flatten \
-	          "$filepath"
+	get_random_color
+	color1="$RCOLOR"
+	convert -size "$size" gradient: \( +clone \) \
+	-background $color1 -compose ModulusAdd -flatten -blur 0x"$blur" \
+	"$filepath"
 }
 
 gradient_angles() {
-		get_random_color
-		color1="$RCOLOR"
-		get_random_color
-		color2="$RCOLOR"
-		get_random_color
-		color3="$RCOLOR"
-		get_random_color
-		color4="$RCOLOR"
-		get_random_color
-		color5="$RCOLOR"
-			get_random_number "360"
-		rotate="$RNUM"
-			get_random_number "60"
-		blur="$RNUM"
-
-  convert -size "$size" gradient:$color1-$color2 \( gradient:$color4-$color5 -rotate $rotate \) \
-					-background $color3 -compose ModulusAdd -blur 0x$blur -flatten \
-          "$filepath"
+	get_random_color
+	color1="$RCOLOR"
+	get_random_color
+	color2="$RCOLOR"
+	get_random_color
+	color3="$RCOLOR"
+	get_random_color
+	color4="$RCOLOR"
+	get_random_color
+	color5="$RCOLOR"
+	get_random_number "360"
+	rotate="$RNUM"
+echo $rotate
+	convert -size "$size" gradient:$color1-$color2 \( gradient:$color4-$color5 -rotate $rotate \) \
+	-background $color3 -compose ModulusAdd -flatten  \
+	"$filepath"
 }
 
-layered_levels() {
-		get_random_color
-		color1="$RCOLOR"
-	convert -size "$size" xc: +noise Random "/tmp/noise.png"
-	convert "/tmp/noise.png" -blur 0x12 -normalize \
-     \( -size 1x9 xc: -draw 'color 0,4 point' \) \
-     -fx '(.5+.3*v.p{0,u*(v.h-1)})' \
-     \( +clone -normalize -edge .3 -fx 'R+G+B' \) \
-     -fx 'intensity+v'  -fill $color1 -tint 100 "$filepath"
-}
+gradient_c() {
 
-edged_level() {
-		get_random_color
-		color1="$RCOLOR"
-convert -size "$size" xc: +noise Random "/tmp/noise.png"
-	convert "/tmp/noise.png" -blur 0x12 -normalize \
-	 \( -size 1x9 xc: -draw 'color 0,4 point' \) \
-	 -fx '(.6+.2*v.p{0,G*(v.h-1)})' \
-	 \( +clone -normalize -edge 1 \) -fill $color1 -tint 100 -fx 'u+v' "$filepath"
+  convert \( xc:red xc:blue +append \) \
+          \( xc:yellow xc:cyan +append \) -append \
+          -filter point -interpolate catrom \
+          -define distort:viewport="100x100" \
+          -distort Affine '.5,.5 .5,.5   1.5,1.5 99.5,99.5' \
+          -resize $size "$filepath"
+
 }
 
 if [ "$pattern" = "random" ]; then
-	get_random_number "11"
+	get_random_number "10"
 	pattern="$RNUM"
 fi
 
 case "$pattern" in
-		1|solid)
-			solid
-			;;
-		2|linear)
-				linear_gradient
-				;;
-		3|radial)
-				radial_gradient
-				;;
-		4|twisted)
-				twisted_gradient
-				;;
-		5|bilinear)
-				bilinear_gradient
-				;;
-		6|plasma)
-				plasma
-				;;
-		7|blurred)
-				blurred_noise
-				;;
-		8|gradient1)
-				gradient_horizontal
-				;;
-		9|gradient2)
-				gradient_angles
-				;;
-		10|layered)
-				layered_levels
-				;;
-		11|edged)
-				edged_level
-				;;
+	1|solid)
+	solid
+	;;
+	2|linear)
+	linear_gradient
+	;;
+	3|radial)
+	radial_gradient
+	;;
+	4|twisted)
+	twisted_gradient
+	;;
+	5|bilinear)
+	bilinear_gradient
+	;;
+	6|plasma)
+	plasma_fractal
+	;;
+	7|blurred)
+	blurred_noise_paint
+	;;
+	8|gradient1)
+	gradient_horizontal
+	;;
+	9|gradient2)
+	gradient_angles
+	;;
+	10|plasma1)
+	plasma_single
+	;;
+	11|plasma2)
+	plasma_range
+	;;
+	12|test)
+	gradient_c
 esac
