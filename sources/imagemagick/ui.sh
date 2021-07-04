dea1d84b8a709115a41a3efaf1f34e719da50e7a
ksyko <ksyko@pm.me>
2021-07-04 23:32:08 +0530

    change to one line

--- sources/imagemagick/ui.sh
+++ sources/imagemagick/ui.sh
@@ -1,9 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # sort
-u_input=$(get_input radio "pattern"
-"solid,linear,radial,twisted,
-bilinear,plasma,blurred,
-gradient1,gradient2,plasma1,plasma2"
-)
+u_input=$(get_input radio "pattern" "solid,linear,radial,twisted,bilinear,plasma,blurred,gradient1,gradient2,plasma1,plasma2")
 config_set "ImageMagick: Choose a pattern" "$u_input"
--- sources/reddit/ui.sh
+++ sources/reddit/ui.sh
@@ -1,16 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # sub
-u_input=$(get_input radio "Reddit: Choose a subreddit"
-"
-iWallpaper,
-MobileWallpaper,
-Verticalwallpapers,
-Amoledbackgrounds,
-AnimePhoneWallpapers,
-ComicWalls,
-wallpaper+wallpapers
-")
+u_input=$(get_input radio "Reddit: Choose a subreddit" "iWallpaper,MobileWallpaper,Verticalwallpapers,Amoledbackgrounds,AnimePhoneWallpapers,ComicWalls,wallpaper+wallpapers")
 config_set "sub" "$u_input"
 
 # sort
