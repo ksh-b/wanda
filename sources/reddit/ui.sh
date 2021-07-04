0b88ebacd61c2a8826d06cfa4a78e1e9c1be413a
ksyko <ksyko@pm.me>
2021-07-04 23:46:22 +0530

    test

--- sources/reddit/ui.sh
+++ sources/reddit/ui.sh
@@ -1,4 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONFIG_FILE="$SCRIPT_DIR/sources/$source/config"
+. "$SCRIPT_DIR/tools/util.sh"
+. "$SCRIPT_DIR/config"
 
 # sub
 u_input=$(get_input radio "Reddit: Choose a subreddit" "iWallpaper,MobileWallpaper,Verticalwallpapers,Amoledbackgrounds,AnimePhoneWallpapers,ComicWalls,wallpaper+wallpapers")
