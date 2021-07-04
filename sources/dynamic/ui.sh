e9bda5d7b5c6e555ba3fec3420684c93e7ff1471
ksyko <ksyko@pm.me>
2021-07-04 13:04:47 +0530

    add ui

---
+++ sources/dynamic/ui.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+CONFIG_FILE=$SCRIPT_DIR/sources/dynamic/config
+. "$CONFIG_FILE"
+. "$SCRIPT_DIR/util.sh"
+
+imagesets=""
+# sort
+for f in "$SCRIPT_DIR"sources/dynamic/images/*; do
+    imagesets="$imagesets""$(basename "$f,")"
+done
+
+u_input=$(get_input radio "Dynamic: Choose a set" "$imagesets")
+config_set "$CONFIG_FILE" "imageset" "$u_input"
