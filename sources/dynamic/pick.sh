e0839959b05d0e426daace97d5dcd4568bb208a9
ksyko <ksyko@pm.me>
2021-07-04 13:04:38 +0530

    add comments

--- sources/dynamic/pick.sh
+++ sources/dynamic/pick.sh
@@ -1,8 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/bash
 . "$SCRIPT_DIR/sources/dynamic/config"
+
+# get hour
 h=$(date +"%-H")
+
+# symlinks dont get extracted from the zip
+# workaround - get the file from latest recent hour
 if [ ! -f "$filepath" ]; then
   h=$((h-1))
 fi
-filepath="$SCRIPT_DIR/dynamic/images/$imageset/$h.jpg"
+
+# apply wallpaper
+filepath="$SCRIPT_DIR/sources/dynamic/images/$imageset/$h.jpg"
 echo "$filepath"
