fc248fca1a790745c343b362305ea4aa19ade43d
ksyko <ksyko@pm.me>
2021-07-04 09:52:55 +0530

    fix wallpaper not set times between 0-9

--- sources/dynamic/pick.sh
+++ sources/dynamic/pick.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 . "$SCRIPT_DIR/dynamic/config"
-h=$(date +"%H")
-filepath="$SCRIPT_DIR/dynamic/images/$imageset/$h.jpg"
+h=$(date +"%-H")
 if [ ! -f "$filepath" ]; then
   h=$((h-1))
 fi
+filepath="$SCRIPT_DIR/dynamic/images/$imageset/$h.jpg"
 echo "$filepath"
