478910c4b2a20cb6d0246f1ab8272c9683b7479a
ksyko <ksyko@pm.me>
2021-07-06 23:47:39 +0530

    fix config not set and limit to page 1

--- sources/4chan/ui.sh
+++ sources/4chan/ui.sh
@@ -6,19 +6,24 @@ u_input=$(get_input "radio" "4chan: Choose board" "wg,w,hr")
 config_set "board" "$u_input"
 
 # thread
-api="https://a.4cdn.org/wg/catalog.json"
+api="https://a.4cdn.org/$u_input/catalog.json"
 res=$(curl -s "$api")
 posts_count=$(echo "$res" | jq '.[0].threads | length')
 posts=""
-threads=$(echo "$res" | jq -cr '.[].threads[] | { no: .no, des: .semantic_url }')
+threads=$(echo "$res" | jq -cr '.[0].threads[] | { no: .no, des: .semantic_url }')
+
+echo "Generating thread list. Please wait."
+i=0
 for thread in $threads; do
+  i=$((i+1))
   n=$(echo "$thread" | jq '.no')
   d=$(echo "$thread" | jq --raw-output '.des')
   posts="${posts}$d $n,"
+  echo "$i/$posts_count"
 done
-IFS=" "
-read -ra strarr <<< "$posts"
-config_set "colors" "${strarr[1]}"
+
 
 u_input=$(get_input "radio" "4chan: Choose thread" "${posts::-1}")
-config_set "board" "$u_input"
+IFS=" "
+read -ra strarr <<< "$u_input"
+config_set "board" "${strarr[1]}"
