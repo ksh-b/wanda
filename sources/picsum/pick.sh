4a694a719a35ff47a5f7492a476c6fad036cd9d2
ksyko <ksyko@pm.me>
2021-07-06 21:56:41 +0530

    add config for picsum

--- sources/picsum/pick.sh
+++ sources/picsum/pick.sh
@@ -1,5 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 seed=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 ; echo '')
-api="https://picsum.photos/seed/$seed/$width/$height"
+params=""
+if [ $grayscale = "true" ]; then
+  params="grayscale"
+fi
+if [ "$blur" -gt "0" ]; then
+  params="$params&blur=$blur"
+fi
+api="https://picsum.photos/seed/$seed/$width/$height?$params"
 url=$(curl -Ls -o /dev/null -w %{url_effective} "$api")
 echo "$url"
