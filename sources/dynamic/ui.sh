6a4aeeae60df599bf97c4f0b1656a7369e9ee61b
ksyko <ksyko@pm.me>
2021-07-06 23:49:21 +0530

    remove last comma

--- sources/dynamic/ui.sh
+++ sources/dynamic/ui.sh
@@ -9,5 +9,5 @@ for f in "$SCRIPT_DIR"sources/dynamic/images/*; do
     imagesets="$imagesets""$(basename "$f,")"
 done
 
-u_input=$(get_input radio "Dynamic: Choose a set" "$imagesets")
+u_input=$(get_input radio "Dynamic: Choose a set" "${imagesets::-1}")
 config_set "imageset" "$u_input"
