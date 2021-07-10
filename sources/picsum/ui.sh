ba17c657330b4caa89107fff2b8f1aca891e8acd
ksyko <ksyko@pm.me>
2021-07-10 17:57:45 +0530

    fix config not set

--- sources/picsum/ui.sh
+++ sources/picsum/ui.sh
@@ -3,8 +3,8 @@
 
 # grayscale
 u_input=$(get_input "radio" "Picsum: Apply grayscale" "true,false")
-config_set "grayscale" "u_input"
+config_set "grayscale" "$u_input"
 
 # blur
 u_input=$(get_input "counter" "Picsum: Apply blur? (0 for off)" "0,10,0")
-config_set "blur" "u_input"
+config_set "blur" "$u_input"
