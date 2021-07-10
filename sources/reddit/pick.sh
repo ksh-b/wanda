cd6015a4323efe9bdf3171f54463e8fa9362fb16
ksyko <ksyko@pm.me>
2021-07-11 00:34:32 +0530

    skip galleries

--- sources/reddit/pick.sh
+++ sources/reddit/pick.sh
@@ -1,9 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+. "$SCRIPT_DIR/tools/util.sh"
+echo "$SCRIPT_DIR"
 api="https://old.reddit.com/r/${sub}/${sort}.json"
 res=$(curl -s -A "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0" "$api")
 posts=25
 rand=$(get_random_number "$posts")
-echo "$rand"
 url=$(echo "$res" | jq --raw-output ".data.children[$rand].data.url")
+while [[ $url == *"/gallery/"* ]]
+do
+  rand=$(get_random_number "$posts")
+  url=$(echo "$res" | jq --raw-output ".data.children[$rand].data.url")
+done
 echo "$url"
