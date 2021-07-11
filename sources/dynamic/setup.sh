230218822935bd510fe007fbdacc266e76d3f3fe
ksyko <ksyko@pm.me>
2021-07-11 11:37:31 +0530

    add messages

--- sources/dynamic/setup.sh
+++ sources/dynamic/setup.sh
@@ -2,8 +2,10 @@
 DYN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  &> /dev/null && pwd )"
 mkdir -p "$DYN_DIR"
 cd "$DYN_DIR" || (echo "Failed to setup" && exit 1)
-curl -L -o "$DYN_DIR/dynamic.zip" "https://github.com/GitGangGuy/dynamic-wallpaper-improved/archive/refs/heads/master.zip" &> /dev/null
+echo "Downloading imagesets... [~180 MB]"
+curl -L -o "$DYN_DIR/dynamic.zip" "https://github.com/GitGangGuy/dynamic-wallpaper-improved/archive/refs/heads/master.zip"
 unzip "$DYN_DIR/dynamic.zip" "dynamic-wallpaper-improved-master/images/*" &> /dev/null
 mv "$DYN_DIR/dynamic-wallpaper-improved-master/images/" "$DYN_DIR"
+echo "Cleaning up..."
 rm -rf "$DYN_DIR/dynamic-wallpaper-improved-master"
 rm "$DYN_DIR/dynamic.zip"
