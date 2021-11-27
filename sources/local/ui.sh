83c4f191111bc647034ac0d5d0242ed8929c19a4
ksyko <ksyko@pm.me>
2021-11-27 16:26:23 +0530

    Initial commit

--- sources/local/ui.sh
+++
@@ -1,6 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-. "$SCRIPT_DIR/tools/util.sh"
-
-# images_path
-u_input=$(get_input "text" "Local: Enter path for wallpapers (from Internal memory)" "Pictures")
-config_set "images_path" "$HOME/storage/shared/$u_input"
