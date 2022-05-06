72b32e2db6645ca7fc5cf94f4834290aef5d573e
kshib <ksyko@pm.me>
2022-05-06 16:29:23 +0530

    bump version

--- wanda/wanda.py
+++ wanda/wanda.py
@@ -16,7 +16,7 @@ from lxml import html
 user_agent = {"User-Agent": "git.io/wanda"}
 content_json = "application/json"
 folder = f'{str(Path.home())}/wanda'
-version = '0.56.1'
+version = '0.56.2'
 
 def parser():
     parser_ = argparse.ArgumentParser(description='Set wallpapers')
