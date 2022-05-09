b4385480ad698e2fa83a72416417c47986109eef
kshib <ksyko@pm.me>
2022-05-09 10:10:21 +0530

    bump version

--- wanda/wanda.py
+++ wanda/wanda.py
@@ -14,7 +14,7 @@ import requests
 from lxml import html
 from wand.image import Image
 
-version = '0.58.1'
+version = '0.58.2'
 
 user_agent = {"User-Agent": "git.io/wanda"}
 content_json = "application/json"
